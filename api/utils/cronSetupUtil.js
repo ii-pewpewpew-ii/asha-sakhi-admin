0046d30b1db600677b5828939d43eb7423f63a9d
Dakshin <dakshinxo@gmail.com>
2025-04-27 23:23:08 +0530

    Commented out cron util

--- api/utils/cronSetupUtil.js
+++ api/utils/cronSetupUtil.js
@@ -3,10 +3,10 @@ const {appointmentHandlers} = require('../handlers'); // import your handlers
 const responseUtil = require("./responseUtil")
 
 const scheduleJobs = (req, res) => {
-    cron.schedule('10 * * * * *', async () => {
-        console.log('[CRON] Triggering Appointment jobs for the day');
-        await appointmentHandlers.sendAppointmentReminders();
-    });
+    // cron.schedule('10 * * * * *', async () => {
+    //     console.log('[CRON] Triggering Appointment jobs for the day');
+    //     await appointmentHandlers.sendAppointmentReminders();
+    // });
     console.log("[CRON] jobs setup")
 
     return responseUtil.getResponse(res, 200, "Cron started successfully")
